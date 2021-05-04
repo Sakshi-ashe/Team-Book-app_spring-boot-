7add71d9c17e6f1c55f0f4e5aa7a918caf5e10bb
Sakshi Gupta <sakshi.gupta3@globallogic.com>
2021-05-04 17:53:15 +0530

    product by keyword,id

--- Ecommerce version 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/SG/ecommerce/config/MyDataRestConfig.java
+++ Ecommerce version 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/SG/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,13 @@
 package com.SG.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,7 +19,19 @@ import com.SG.ecommerce.entity.ProductCategory;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
 
-	 @Override
+	//@ToDo: our rest api disclose the entity id
+	private EntityManager entityManager;
+	
+	
+	//#################Autowire JPA entity manager########
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
+	
+	
+	
+	@Override
 	    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
 	        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
@@ -28,6 +48,32 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
 	                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 	                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 	        
+	        
+	        //call an internal helper method to expose the id's
+	        
+	        exposeIds(config);
 	 }
+	
+	
+	private void exposeIds(RepositoryRestConfiguration config) {
+		
+		//expose entity ids
+		
+		
+		//get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		//create an array List of those entity types
+		List<Class> entityClasses = new ArrayList<>();
+		for(EntityType tempEntityType: entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+		
+		//expose the entity id for the array of entity/domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes); //this method expose id's exposeIdsFor
+		
+		
+	}
 	
 }
--- Ecommerce version 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/SG/ecommerce/dao/ProductRepository.java
+++ Ecommerce version 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/SG/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,20 @@
 package com.SG.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.SG.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 // why not here @Repository resource
 public interface ProductRepository extends JpaRepository<Product,Long>{
+	
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+		
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
